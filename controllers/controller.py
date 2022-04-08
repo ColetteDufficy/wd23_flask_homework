0a9938d075f8007b31c510efef1ee2cb2b8c412a
colette dufficy <c.dufficy@gmail.com>
2022-04-08 14:42:57 +0100

    MVP completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 from models.event_list import events
@@ -6,8 +6,21 @@ from models.event import Event
 
 @app.route('/')
 def index():
-    return "Welcome to my home page"
+    return render_template("index.html", title="Home ")
 
 @app.route('/events')
 def list_events():
-    return render_template("index.html", title="Todays ", all_events = events)
+    return render_template("event_listing.html", title="Todays ", all_events = events)
+
+@app.route('/events', methods=["POST"])
+def add_event():
+    ev_date = request.form['date']
+    ev_event_name = request.form['title']
+    ev_num_of_guests = request.form['guest numbers']
+    ev_location = request.form['location']
+    ev_descripton = request.form['description']
+        
+    new_event = Event(ev_date, ev_event_name, ev_num_of_guests, ev_location, ev_descripton)
+    events.append(new_event)
+    
+    return render_template("event_listing.html", title="Todays ", all_events = events)
